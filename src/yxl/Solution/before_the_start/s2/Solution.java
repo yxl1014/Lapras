d90b1964d27f69272c09410e93ec551109f70ba6
yxl1014 <1627419257@qq.com>
2022-06-23 20:10:45 +0800

    s2

---
+++ src/yxl/Solution/before_the_start/s2/Solution.java
@@ -0,0 +1,10 @@
+package yxl.Solution.before_the_start.s2;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+}
